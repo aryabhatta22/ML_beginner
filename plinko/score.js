978ccf9e38283839bc8c60ce048bf055c36d4550
Tarun Sharma <tarun.hive@hotmail.com>
2019-06-15 19:29:20 +0530

    knn algorithm implemented

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,32 @@
+const output=[];
+const predictionPoint = 300;
+const k = 3;
+
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  output.push([dropPosition,bounciness,size,bucketLabel]);
+    
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  
+    const bucket = _.chain(output)
+                    .map(row => [distance(row[0]), row[3] ])
+                    .sortBy(row => row[0])
+                    .slice(0,k)
+                    .countBy(row=>row[1])
+                    .toPairs()
+                    .sortBy(row => row[1])
+                    .last()
+                    .first()
+                    .parseInt()
+                    .value();
+
+    console.log('The ball is likely to fall in the bucket #'+bucket+' if dropped from '+predictionPoint+'px');
+    
+    
 }
 
+function distance(point) {
+    return Math.abs(point - predictionPoint);
+}
